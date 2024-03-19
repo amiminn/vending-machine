8394d7f2f2a84ce6d90750b7fbe204fb90ed7dc7
amiminn <tholabul.amin@gmail.com>
2024-03-19 14:20:53 +0700

    update callback

---
+++ resources/js/components/callback.vue
@@ -0,0 +1,50 @@
+<template lang="">
+    <div v-if="show">
+        <div class="flex justify-between mb-1">
+            <span> </span>
+            <span class="text-sm font-bold text-bg">
+                {{ setting.indexMulai }}%
+            </span>
+        </div>
+        <div class="w-full bg-gray-200 rounded-full h-3">
+            <div
+                class="bg-emerald-400 h-3 rounded-full"
+                :style="{ width: setting.indexMulai + '%' }"
+            ></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            show: false,
+            setting: {
+                indexMulai: 0,
+                timeout: 2,
+                hide_after: 3,
+            },
+        };
+    },
+    methods: {
+        start() {
+            this.show = true;
+            const interval = setInterval(() => {
+                this.setting.indexMulai = (this.setting.indexMulai % 100) + 1;
+                if (this.setting.indexMulai === 100) {
+                    clearInterval(interval);
+                    setTimeout(() => {
+                        this.show = false;
+                    }, 2000);
+                }
+            }, (this.setting.timeout * 1000) / 100);
+        },
+    },
+    mounted() {
+        Echo.channel("chat").listen(".bubble.chat", (e) => {
+            this.start();
+        });
+    },
+};
+</script>
+<style lang=""></style>
